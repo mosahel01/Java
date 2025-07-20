5c701bc4695acc4e79248271f57d701a38d8e49f
mosahel01 <hoywd@gmail.com>
2025-07-20 23:04:49 +0530

    System.out.println("First commit");

---
+++ 01-Java-Basics/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Swap 2 variables in Java
+        String x = "water";
+        String y = "cool-aid";
+        String temp = " ";
+
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("x :" + x);
+        System.out.println("y :" + y);
+        System.out.println("Hello world");
+
+    }
+}
---
+++ 01-Java-Basics/src/Notes.java
@@ -0,0 +1,15 @@
+public class Notes {
+    public static void JavaNotes(String[] args) {
+        System.out.print("Notes of what i learn in Java ");
+
+
+//    here we write our notes
+//        1. class name at line 1 should match file name
+//        2. myClass & MyClass are different || Java is case-sensitive
+//        3. Println vs Print => Print does not insert a new line at the end of the output
+//        4. type valueName = value;  || that's how you declare variables in Java
+
+
+
+    }
+}

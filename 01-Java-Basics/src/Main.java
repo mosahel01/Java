6df5a609d5a8d7b0dde251291e074e7e2187d20a
mosahel01 <hoywd@gmail.com>
2025-07-21 10:55:54 +0530

    System.out.print("Variables");

--- 01-Java-Basics/src/Main.java
+++ 01-Java-Basics/src/Main.java
@@ -1,18 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        // Swap 2 variables in Java
-        String x = "water";
-        String y = "cool-aid";
-        String temp = " ";
+        int maxScore = 51;
+        int minScore = 24;
 
-        temp = x;
-        x = y;
-        y = temp;
+        // we can assign double a float, but not the other way around.
+        double percent = (float) minScore / maxScore * 100;
 
-        System.out.println("x :" + x);
-        System.out.println("y :" + y);
-        System.out.println("Hello world");
+        System.out.println(percent);
+
+
+//        # HOW TO TAKE INPUT
+//        Scanner scan = new Scanner(System.in);
+
+//        System.out.println("What is your name ? ");
+//        String scann = scan.nextLine();
+//        System.out.println("How old are you ? ");
+//        int age = scan.nextInt();
+//        System.out.println("What is your favourite food ? ");
+//        String food = scan.nextLine();
+
+
+//        System.out.println("Hello " + scann);
+//        System.out.println("You are " + age + " years old");
+//        System.out.println(food + " is your favourite food");
+
+
+//        # Swap 2 variables in Java
+//        String x = "water";
+//        String y = "cool-aid";
+//        String temp = " ";
+//
+//        temp = x;
+//        x = y;
+//        y = temp;
+//
+//        System.out.println("x :" + x);
+//        System.out.println("y :" + y);
+//        System.out.println("Hello world");
 
     }
 }

7095cb216d5b16bf72ac909656ca41926fea6e3b
mosahel01 <hoywd@gmail.com>
2025-07-22 20:43:37 +0530

    Projects

--- 03FirstProject/src/Main.java
+++ 03FirstProject/src/Main.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // System.out.println("Hello world");
-
         // Mad libs game
 
         Scanner scanner = new Scanner(System.in);

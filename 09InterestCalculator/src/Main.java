c344d72c911acaf781a44ac260fe179bbd0f8c97
mosahel01 <hoywd@gmail.com>
2025-07-24 10:28:30 +0530

    Compound interest calculator

--- 09InterestCalculator/src/Main.java
+++ 09InterestCalculator/src/Main.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Compound interest calculator (astagfirullah)
+        // Compound interest calculator [astagfirullah]
         Scanner scanner = new Scanner(System.in);
 
-        double principal; // initial money we have
-        double rate; // interest rate on principal
-        double amount; // total amount in the end
-        int timesCompounded; // n = 1 or n = years, if its compounded once a year
-        int years; // number of years investment/loan is held
+        double principal;        // initial money we have
+        double rate;             // interest rate on principal
+        double amount;           // total amount in the end
+        int timesCompounded;     // n = 1 or n = years, if its compounded once a year
+        int years;               // number of years investment/loan is held
 
         System.out.print("Enter the principal amount: ");
         principal = scanner.nextDouble();

4fb101d50eb09b988e0635400f68208ef26f7268
mosahel01 <hoywd3465@gmail.com>
2025-07-27 08:18:28 +0530

    Calculator Initialised

--- 11TemperatureConverter/src/Main.java
+++ 11TemperatureConverter/src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
         // TEMPERATURE CONVERSION PROGRAM
 
         Scanner input = new Scanner(System.in);
@@ -15,15 +14,16 @@ public class Main {
         temperature = input.nextDouble();
 
         System.out.print("Convert to Celsius or Fahrenheit? (C or F): ");
-        scale = input.next().toUpperCase();        // method chaining
+        scale = input.next().toUpperCase(); // method chaining
 
         newTemp = (scale.equals("C")) ? (temperature - 32) * 5 / 9 : (temperature * 9 / 5 + 32);
 
-//        System.out.printf("newTemp + "°" + scale");
+        // System.out.printf("newTemp + "°" + scale");
         System.out.printf("%.2f°%s", newTemp, scale);
 
+        System.out.println("Kanan => Armenian Military ");
 
         input.close();
-
     }
 }
+

98f842f032060b16fcac1ad35b02412620fa9d85
mosahel01 <hoywd3465@gmail.com>
2025-07-27 18:19:39 +0530

    Functionality added

--- 12JavaCalculatorr/src/Main.java
+++ 12JavaCalculatorr/src/Main.java
@@ -1,10 +1,29 @@
-public class Main {
-    public statis
+import java.util.Scanner;
 
-    void main(String[] args) {
+public class Main {
+    public static void main(String[] args) {
 
 
         // CALCULATOR PROGRAM
+        Scanner scanner = new Scanner(System.in);
+
+        double num1;
+        double num2;
+        char operator;
+        double result;
+
+
+        System.out.println("Enter the first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.println("Enter an Operator(+, -, *, /, ^): ");
+        operator = scanner.next().charAt(0);
+
+        System.out.println("Enter the second number: ");
+        num2 = scanner.nextDouble();
+
+
+        scanner.close();
 
 
     }

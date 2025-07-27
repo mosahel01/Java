43f30a169187f2cb96a9657bd741dd15ae55a65a
mosahel01 <hoywd3465@gmail.com>
2025-07-27 08:17:44 +0530

    Enhanced Switch Statements

--- 05Iterators/src/SwitchStatement.java
+++ 05Iterators/src/SwitchStatement.java
@@ -8,39 +8,20 @@ public class SwitchStatement {
 
         String day;
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a day: ");
         day = scanner.nextLine();
 
 
         switch (day) {
-            case "Monday" -> System.out.println("It is a weekday");
-            case "Tuesday" -> System.out.println("It is a weekday");
-            case "Wednesday" -> System.out.println("It is a weekday");
-            case "Thursday" -> System.out.println("It is a weekday");
-            case "Saturday" -> System.out.println("It is a weekend, yay!!");
-            case "Sunday" -> System.out.println("It is a weekend, yay!!");
-            default -> System.out.printf("%s is not a valid day", day);
+            case "Monday", "Tuesday", "Wednesday", "Thursday" -> System.out.println("It is a weekday");
+            case "Saturday", "Sunday" -> System.out.println("It is the weekend");
+            default -> {
+                System.out.printf("%s is not a valid day", day);
+
+                scanner.close();
+            }
+
+
         }
-
-
-//        // This is nested if-else hell
-//        if (day.equals("Monday")) {
-//            System.out.println("It is a weekday");
-//        } else if (day.equals("Tuesday")) {
-//            System.out.println("It is a weekday");
-//        } else if (day.equals("Wednesday")) {
-//            System.out.println("It is a weekday");
-//        } else if (day.equals("Thursday")) {
-//            System.out.println("It is a weekday");
-//        } else if (day.equals("Saturday")) {
-//            System.out.println("It is a weekday");
-//        } else if (day.equals("Sunday")) {
-//            System.out.println("It is a weekday");
-//        } else {
-//            System.out.printf("%s is not a valid day", day);
-//        }
-
-        scanner.close();
-
-
     }
 }

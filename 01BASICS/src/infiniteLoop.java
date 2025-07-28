6781e562a68a533781fbd29298cea13575c7d031
mosahel <hoywd3465@gmail.com>
2025-07-28 23:07:38 +0530

    infinite loops in java

--- 01BASICS/src/infiniteLoop.java
+++ 01BASICS/src/infiniteLoop.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class infiniteLoop {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        String response = "";
+
+        while (!response.equals("Q")) {
+            System.out.println("You are playing a Game");
+            System.out.print("Press Q to quit: ");
+            response = scanner.next().toUpperCase();
+        }
+
+        System.out.println("You have quit the Game");
+
+
+        scanner.close();
+
+
+    }
 }

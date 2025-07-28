44dd9f9d31f1ef9b4a447864225255c1ed45dde1
mosahel <hoywd3465@gmail.com>
2025-07-28 23:07:24 +0530

    Do while in java

--- 01BASICS/src/DoWhile.java
+++ 01BASICS/src/DoWhile.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class DoWhile {
+    public static void main(String[] args) {
+        // Do While LOOP
+        Scanner scanner = new Scanner(System.in);
+
+        String response = "";
+
+        // Do runs code then while checks for condition
+        do {
+            System.out.println("You are playing a Game");
+            System.out.print("Press Q to quit: ");
+            response = scanner.next().toUpperCase();
+
+        } while (!response.equals("Q"));
+
+        System.out.println("You have quit the Game");
+
+
+        scanner.close();
+    }
 }

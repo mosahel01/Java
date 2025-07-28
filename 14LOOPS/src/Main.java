e6bdd431e264494db13383eeb2b34785cf42466b
mosahel <hoywd3465@gmail.com>
2025-07-28 23:08:18 +0530

    Loops in Java

---
+++ 14LOOPS/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Loops - execute some code for certain amount of time
+
+
+       // FOR loop => ( initialisation/initial value, when do we want to stop , change initial value by some method )
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Pizza");
+        }
+
+
+    }
+}

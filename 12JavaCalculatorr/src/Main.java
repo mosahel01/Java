aa28bcd77fc9c96ec10f0e54b5e0ba4efffcc17c
mosahel <hoywd3465@gmail.com>
2025-07-28 09:58:40 +0530

    Calculator with Enhanced Switches

--- 12JavaCalculatorr/src/Main.java
+++ 12JavaCalculatorr/src/Main.java
@@ -10,7 +10,8 @@ public class Main {
         double num1;
         double num2;
         char operator;
-        double result;
+        double result = 0;
+        boolean validOperation;
 
 
         System.out.println("Enter the first number: ");
@@ -22,6 +23,29 @@ public class Main {
         System.out.println("Enter the second number: ");
         num2 = scanner.nextDouble();
 
+
+        switch (operator) {
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> {
+                if (num2 == 0) {
+                    System.out.println("Cannot divide by Zero");
+                    validOperation = false;
+                } else {
+                    result = num1 / num2;
+                }
+            }
+            case '^' -> result = Math.pow(num1, num2);
+            default -> {
+                System.out.println("Invalid Operator");
+                validOperation = false;
+            }
+        }
+
+        if (validOperation) {
+            System.out.println(result);
+        }
 
         scanner.close();
 

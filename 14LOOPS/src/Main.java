d8f43e096be2858434dea6496f4a9408d923c0bf
mosahel <hoywd3465@gmail.com>
2025-07-29 09:36:25 +0530

    Loopss

--- 14LOOPS/src/Main.java
+++ 14LOOPS/src/Main.java
@@ -1,13 +1,25 @@
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // Loops - execute some code for certain amount of time
 
+        // FOR loop => ( initialisation/initial value, when do we want to stop , change initial value by some method )
 
-       // FOR loop => ( initialisation/initial value, when do we want to stop , change initial value by some method )
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Pizza");
+//        for (int i = 0; i < 20; i = i + 2) {
+//            // System.out.println("Pizza");
+//            System.out.print(" " + i);
+//        }
+
+
+        int start = 10;
+
+        for (int i = start; i > 0; i--) {
+            System.out.println(i);
+            Thread.sleep(1000);
         }
+
+        System.out.println("Happy Birthday 2u");
 
 
     }

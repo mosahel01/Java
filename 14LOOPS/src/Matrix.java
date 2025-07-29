c247dc0da76f977011d1fa0a83564ed3d611b23e
mosahel <hoywd3465@gmail.com>
2025-07-29 09:36:40 +0530

    For Loop Matrix

--- 14LOOPS/src/Matrix.java
+++ 14LOOPS/src/Matrix.java
@@ -1,2 +1,33 @@
+import java.util.Scanner;
+
 public class Matrix {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        int rows;
+        int columns;
+        char symbol;
+
+        System.out.print("Enter the # of rows: ");
+        rows = scanner.nextInt();
+
+        System.out.print("Enter the # of columns: ");
+        columns = scanner.nextInt();
+
+        System.out.print("Enter the symbol to use: ");
+        symbol = scanner.next().charAt(0);
+
+
+        for (int j = 0; j < columns; j++) {
+            for (int i = 0; i < rows; i++) {
+                System.out.print(symbol + " ");
+            }
+            System.out.println(symbol + " ");
+        }
+
+
+        scanner.close();
+    }
 }

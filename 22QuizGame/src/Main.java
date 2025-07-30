e6bb18ba3a78dffd4fb2c5589f69c2f7a3ada0c0
mosahel <hoywd3465@gmail.com>
2025-07-30 21:02:13 +0530

    Quiz Game

--- 22QuizGame/src/Main.java
+++ 22QuizGame/src/Main.java
@@ -6,19 +6,24 @@ public class Main {
         // Java Quiz Game
 
         String[] questions = {
-                "What is the main function of a router ? ",
-                "Which part of the computer is considered the RAM ? ",
-                "What year was facebook launched ? ",
-                "Who is known as the father of computers ? ",
-                "What was the first programming language ? "
+            "What is the main function of a router ? ",
+            "Which part of the computer is considered the RAM ? ",
+            "What year was facebook launched ? ",
+            "Who is known as the father of computers ? ",
+            "What was the first programming language ? "
         };
 
         String[][] options = {
-                {"1. storing files", "2. encrypting data", "3. directing internet traffic", "4. managing passwords"},
-                {"1. CPU", "2. hard drive", "3. ram", "4. gpu"},
-                {"1. 2000", "2. 2004", "3. 2006", "4. 2008"},
-                {"1. steve jobs", "2. bill gates", "3. alan turing", "4. charles babbage"},
-                {"1. cobol", "2. c", "3. fortran", "4. assembly"},
+            {
+                "1. storing files",
+                "2. encrypting data",
+                "3. directing internet traffic",
+                "4. managing passwords"
+            },
+            {"1. CPU", "2. hard drive", "3. ram", "4. gpu"},
+            {"1. 2000", "2. 2004", "3. 2006", "4. 2008"},
+            {"1. steve jobs", "2. bill gates", "3. alan turing", "4. charles babbage"},
+            {"1. cobol", "2. c", "3. fortran", "4. assembly"},
         };
 
         int[] answers = {3, 1, 2, 4, 3};
@@ -31,9 +36,8 @@ public class Main {
         System.out.println("Welcome to the Java Quiz Game");
         System.out.println("*****************************");
 
-
         for (int i = 0; i < questions.length; i++) {
-            System.out.println("\n"+questions[i]);
+            System.out.println("\n" + questions[i]);
             for (String option : options[i]) {
                 System.out.println(option);
             }
@@ -52,7 +56,6 @@ public class Main {
         }
 
         System.out.println("Your score will be " + score + " out of " + questions.length);
-
 
         scanner.close();
     }

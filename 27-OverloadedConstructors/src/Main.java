cacc1245437702cd4097019cd58f02afacb4cb94
mosahel <hoywd3465@gmail.com>
2025-08-02 07:20:48 +0530

    Overloaded constructors

---
+++ 27-OverloadedConstructors/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Overloaded Constructors -> Allow a Class to have multiple Constructors
+        // with different params list, enabling objects to initialised in various ways
+
+        User user1 = new User("Spongebob");
+        User user2 = new User("Spaghetti", "Italian@Mafia.com");
+        User user3 = new User("Mos", "dawood@ibrahim.com", 21);
+        User user4 = new User();
+
+        System.out.print(user1.username + ", ");
+        System.out.print(user1.email + ", ");
+        System.out.println(user1.age);
+
+        System.out.print(user2.username + ", ");
+        System.out.print(user2.email + ", ");
+        System.out.println(user2.age);
+
+        System.out.print(user3.username + ", ");
+        System.out.print(user3.email + ", ");
+        System.out.println(user3.age);
+
+        System.out.print(user4.username + ", ");
+        System.out.print(user4.email + ", ");
+        System.out.println(user4.age);
+
+    }
+
+}
--- 27-OverloadedConstructors/src/User.java
+++ 27-OverloadedConstructors/src/User.java
@@ -1,2 +1,31 @@
 public class User {
+
+    String username;
+    String email;
+    int age;
+
+    User() {
+        this.username = "Guest";
+        this.email = "Default User";
+        this.age = 0;
+    }
+
+    User(String username) {
+        this.username = username;
+        this.email = email;
+        this.age = 0;
+    }
+
+    User(String username, String email) {
+        this.username = username;
+        this.email = "User with Mail";
+        this.age = age;
+    }
+
+    User(String username, String email, int age) {
+        this.username = username;
+        this.email = "User with Mail n Age";
+        this.age = 69;
+    }
+
 }

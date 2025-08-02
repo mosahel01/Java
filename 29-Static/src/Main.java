eb9aaa19bb39989d1b6c3a7a3c32b67ea37149e1
mosahel <hoywd3465@gmail.com>
2025-08-02 22:43:55 +0530

    Static Keyword

---
+++ 29-Static/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Static -> makes a Variable/Method belong to the class
+        // rather any specific object
+
+        // commonly used for utility methods or shared resources.
+
+        Friend friend1 = new Friend("SpongeBob");
+
+        System.out.println(friend1.NumOfFriend);
+
+    }
+
+}

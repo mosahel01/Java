479a6077ef1dc7c670a465a39dc98205d8036723
mosahel <hoywd3465@gmail.com>
2025-08-09 19:00:52 +0530

    Static Keyword

--- 29-Static/src/Friend.java
+++ 29-Static/src/Friend.java
@@ -1,11 +1,15 @@
 public class Friend {
 
+    static int NumOfFriend;
     String name;
-    int NumOfFriend;
 
     Friend(String name) {
         this.name = name;
         NumOfFriend++;
+    }
+
+    static void showFriends() {
+        System.out.println("You have " + NumOfFriend + " Friends");
     }
 
 }
--- 29-Static/src/Main.java
+++ 29-Static/src/Main.java
@@ -8,8 +8,15 @@ public class Main {
         // commonly used for utility methods or shared resources.
 
         Friend friend1 = new Friend("SpongeBob");
+        Friend friend2 = new Friend("Mooooos");
+        Friend friend3 = new Friend("Squid");
+        Friend friend4 = new Friend("Narcos");
 
-        System.out.println(friend1.NumOfFriend);
+        System.out.println(Friend.NumOfFriend);
+        Friend.showFriends();
+        // System.out.println(friend1.NumOfFriend);
+        // System.out.println(friend2.NumOfFriend);
+        // System.out.println(friend3.NumOfFriend);
 
     }
 

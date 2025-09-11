aebdea856e82fc2f7493ea3df895f458ce2f37db
mosahel01 <mos.codes1@gmail.com>
2025-09-11 12:51:45 +0530

    Wrapper classes

--- 41-WrapperClasses/src/Main.java
+++ 41-WrapperClasses/src/Main.java
@@ -7,19 +7,34 @@ public class Main {
         //                    Allows use of Collections Frameworks and static Utility Methods.
 
 
+        // Autoboxing
+        // Integer a = new Integer(6969);
+        // Double b = new Double(3.14);
+        // Character c = new Character("$");
+        // Boolean d = new Boolean(true);
+        // String e = new String("Pizza");
 
-        Integer a = new Integer(6969);
+        // Integer a = 6969;
+        // Double b = 3.14;
+        // Character c = '$';
+        // Boolean d = true;
+        // String e = new String("Pizza");
+
+        // // Unboxing
+        // int u = a;
+        // double v = b;
+        // char x = c;
+        // boolean y = d;
+        // String z = e;
 
 
+        // Integer myInt = 4;
+        // System.out.println(myInt);
 
+        char x = 'b';
 
-
-
-
-
-
-
-
+        System.out.println(Character.isLetter(x));
+        System.out.println(Character.isUpperCase(x));
 
 
     }

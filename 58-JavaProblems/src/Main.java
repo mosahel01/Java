c802be28304477708d18ec9752900325107dcab8
mosahel01 <mos.codes1@gmail.com>
2025-10-03 08:05:15 +0530

    Java Problem Solving

---
+++ 58-JavaProblems/src/Main.java
@@ -0,0 +1,19 @@
+void main() {
+    // Coding Bat Problem Solving
+    IO.println("Hola socio");
+    doubleX("abaxxbsdcxcxlkjaxx xclkj kljxcxxjkj");
+}
+
+int doubleX(String str) {
+    for (int i = 0; i < 9; i++) {
+        if (okay) {
+            System.out.println("heol");
+        }
+    }
+
+    int result = str.indexOf("x");
+    System.out.println(result);
+    return result;
+}
+
+

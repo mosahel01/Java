d611a487e4b19d605b7e86d8693b927f6a74d27e
mos069 <user.mos013@gmail.com>
2025-10-14 20:16:37 +0530

    Rock Paper Scissor

---
+++ 58-JavaProblems/src/LinkedLists.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+
+
+public class LinkedLists {
+    public static void main(String[] args) {
+
+
+        // LinkedList
+
+        LinkedList<String> linked = new LinkedList<>();
+
+
+
+    }
+}
+
+
+
+
+
+
